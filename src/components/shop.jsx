2bec5ec3baaf7b32c79806484a42358d6bc0941b
Tarique <Tariqueelshamy11@gmail.com>
2025-07-09 10:44:24 -0400

    test

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -54,7 +54,7 @@ const Shop = () => {
               <img src="/Images/jamie-street-VETPuVqsqnQ-unsplash.jpg" className="d-block w-100 object-fit-cover" alt="Pet 3" />
             </div>
             <div className="carousel-item">
-              <img src="https://place-puppy.com/803x400" className="d-block w-100 object-fit-cover" alt="Pet 4" />
+              <img src="/Images/maurice.jpg className="d-block w-100 object-fit-cover" alt="Pet 4" />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#storeCarousel" data-bs-slide="prev">
